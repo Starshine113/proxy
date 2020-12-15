28b3f1f0e66b9b5b544a2a47794bc2f7af1cda07
Starshine System <us@starshines.xyz>
2020-12-15 21:02:15 +0100

    update status every minute

--- bot/status.go
+++ bot/status.go
@@ -3,6 +3,7 @@ package bot
 import (
 	"fmt"
 	"github.com/bwmarrin/discordgo"
+	"time"
 )
 
 func (b *Bot) Ready(_ *discordgo.Session, _ *discordgo.Ready) {
@@ -10,4 +11,13 @@ func (b *Bot) Ready(_ *discordgo.Session, _ *discordgo.Ready) {
 	if err != nil {
 		b.Sugar.Errorf("Error updating status: %v", err)
 	}
+
+	// update every minute
+	go func() {
+		time.Sleep(time.Minute)
+		err := b.Session.UpdateStatus(0, fmt.Sprintf("%vhelp | in %v servers", b.Prefix, len(b.Session.State.Guilds)))
+		if err != nil {
+			b.Sugar.Errorf("Error updating status: %v", err)
+		}
+	}()
 }
